37cc49b33713a3530a5575468338ccaf9c174212
LukeWalls22 <dimuroluca@hotmail.it>
2024-02-07 17:22:52 +0100

    Base progetto

---
+++ Components/Footer.php
@@ -0,0 +1,20 @@
+  
+        <footer>
+
+        <div class="card text-center">
+            <div class="card-header">
+                Coded by Luca Di Muro
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Epic Library</h5>
+                <p class="card-text mb-0">Progetto settimanale del corso Full Stack Developer di Epicode fatto con PHP e SQL</p>
+                <p class='card-text disc text-secondary'>Le copertine dei libri presenti sono ottenute attraverso percorsi dal sito <a href="https://www.goodreads.com">GoodReads</a>.</p>
+            </div>
+        </div>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+        </footer>
+
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,114 @@
+  <!-- Nome DB: test_epicode_11test_epicode_11 -->
+
+<?php
+
+  require_once('Components/Header.php');
+
+?>
+
+<!-- Inizio Body HTML -->
+
+  <main>
+    <article>
+
+        <h1 class="text-center mt-3 mb-2">Benvenuto in EpicLibrary</h1>
+        <h2 class="text-center mb-5 fs-3">Cerca qui i tuoi libri preferiti</h2>
+
+        <div class="card m-4">
+            <div class="card-body">
+                
+                <h3 class='fs-5 mt-1 mb-4'>Alcuni dei libri che puoi trovare qui:</h3>
+
+                <div class='d-flex justify-content-around flex-wrap'>
+
+                    <div class="card my-3" style="width: 25rem;">
+                        <div class='d-flex p-1'>
+                            <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1671660198i/25454056.jpg" alt="Book Cover" class='align-self-center ms-2 imgSize border'>
+                            <div class="card-body px-2">
+                                <h4 class="card-title my-1">La Spada del destino</h4>
+                                <h5 class='fs-6'><span class='fw-normal'>di</span> Andrzej Sapkowski</h5>
+                                <p class="card-text ps-1">Geralt is a witcher, a man whose magic powers, enhanced by long training and a mysterious elixir,
+                                     have made him a brilliant fighter and a merciless assassin.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card my-3" style="width: 25rem;">
+                        <div class='d-flex p-1'>
+                            <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1671660198i/25454056.jpg" alt="Book Cover" class='align-self-center ms-2 imgSize border'>
+                            <div class="card-body px-2">
+                                <h4 class="card-title my-1">La Spada del destino</h4>
+                                <h5 class='fs-6'><span class='fw-normal'>di</span> Andrzej Sapkowski</h5>
+                                <p class="card-text ps-1">Geralt is a witcher, a man whose magic powers, enhanced by long training and a mysterious elixir,
+                                     have made him a brilliant fighter and a merciless assassin.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card my-3" style="width: 25rem;">
+                        <div class='d-flex p-1'>
+                            <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1671660198i/25454056.jpg" alt="Book Cover" class='align-self-center ms-2 imgSize border'>
+                            <div class="card-body px-2">
+                                <h4 class="card-title my-1">La Spada del destino</h4>
+                                <h5 class='fs-6'><span class='fw-normal'>di</span> Andrzej Sapkowski</h5>
+                                <p class="card-text ps-1">Geralt is a witcher, a man whose magic powers, enhanced by long training and a mysterious elixir,
+                                     have made him a brilliant fighter and a merciless assassin.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card my-3" style="width: 25rem;">
+                        <div class='d-flex p-1'>
+                            <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1671660198i/25454056.jpg" alt="Book Cover" class='align-self-center ms-2 imgSize border'>
+                            <div class="card-body px-2">
+                                <h4 class="card-title my-1">La Spada del destino</h4>
+                                <h5 class='fs-6'><span class='fw-normal'>di</span> Andrzej Sapkowski</h5>
+                                <p class="card-text ps-1">Geralt is a witcher, a man whose magic powers, enhanced by long training and a mysterious elixir,
+                                     have made him a brilliant fighter and a merciless assassin.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card my-3" style="width: 25rem;">
+                        <div class='d-flex p-1'>
+                            <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1671660198i/25454056.jpg" alt="Book Cover" class='align-self-center ms-2 imgSize border'>
+                            <div class="card-body px-2">
+                                <h4 class="card-title my-1">La Spada del destino</h4>
+                                <h5 class='fs-6'><span class='fw-normal'>di</span> Andrzej Sapkowski</h5>
+                                <p class="card-text ps-1">Geralt is a witcher, a man whose magic powers, enhanced by long training and a mysterious elixir,
+                                     have made him a brilliant fighter and a merciless assassin.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card my-3" style="width: 25rem;">
+                        <div class='d-flex p-1'>
+                            <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1671660198i/25454056.jpg" alt="Book Cover" class='align-self-center ms-2 imgSize border'>
+                            <div class="card-body px-2">
+                                <h4 class="card-title my-1">La Spada del destino</h4>
+                                <h5 class='fs-6'><span class='fw-normal'>di</span> Andrzej Sapkowski</h5>
+                                <p class="card-text ps-1">Geralt is a witcher, a man whose magic powers, enhanced by long training and a mysterious elixir,
+                                     have made him a brilliant fighter and a merciless assassin.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+
+            </div>
+        </div>
+
+    </article>
+  </main>
+
+<!-- Fine Body HTML -->
+
+<?php
+    require_once('Components/Footer.php');
+?>
